a5d1fd702fdd49a47c0c52d46ce4aa607a850588
WashingtonlGarcia <washingtonlgarcia@gmail.com>
2020-08-20 13:17:24 -0300

    A new controller was created to call the cloud functions of the firebase API

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,10 +1,5 @@
-import 'dart:convert';
-
-import 'package:cloud_functions/cloud_functions.dart';
 import "package:flutter/material.dart";
-import "package:news_flow/controllers/twitter_controller.dart";
-import "package:http/http.dart" as http;
-import "dart:async";
+import "package:news_flow/controllers/firebase_controller.dart";
 
 class HomePage extends StatelessWidget {
   static const String routeName = "/";
@@ -16,15 +11,8 @@ class HomePage extends StatelessWidget {
       body: _body(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          http.post("endpoint", body: {
-            "message":
-                "Real-time #oilprice news & analysis on #oilmarkets from S&P Global Platts. Covers global #crudeoil, refined oil, #jetfuel & gasoil. RTs don’t equal endorsements",
-          })
-              /*   CloudFunctions.instance.getHttpsCallable(functionName: "function-2").call()*/ .then((value) {
-            var res = json.encode(value.body);
-            print("res= " + res);
-          }).catchError((err) {
-            print(err);
+          FirebaseController().callTest("test").then((dynamic value) {
+            debugPrint(value.toString());
           });
         },
       ),
